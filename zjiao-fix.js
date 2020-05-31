29bc79caf08ef05e65b42f9e76e2d241bb6412c2
xyqyear <215908558@qq.com>
2020-05-31 16:43:11 +0800

    added update link

--- zjiao-fix.js
+++ zjiao-fix.js
@@ -3,7 +3,9 @@
 // @version      0.1
 // @description  fix zjiao.com compatibility shit
 // @author       xyqyear
-// @homepage     https://github.com/xyqyear/zjiao-fix
+// @updateURL    https://cdn.jsdelivr.net/gh/xyqyear/zjiao-fix@master/zjiao-fix.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/xyqyear/zjiao-fix@master/zjiao-fix.js
+// @homepage     https://github.com/xyqyear
 // @match        *://www.zjiao.com/student/course.jsp*
 // @grant        none
 // @run-at       document-start
